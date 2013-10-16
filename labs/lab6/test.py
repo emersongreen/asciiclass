2c6f7c1f59f2161dec34ada30635bdf3e61744ca
Brian Bell <brian.bell.jr@gmail.com>
2013-10-15 21:24:21 -0400

    try to do idf

--- labs/lab6/test.py
+++ labs/lab6/test.py
@@ -8,22 +8,23 @@ sc = SparkContext("spark://ec2-54-200-174-121.us-west-2.compute.amazonaws.com:70
 # Replace `lay-k.json` with `*.json` to get a whole lot more data.
 lay = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/enron/lay-k.json')
 
-json_lay = lay.map(lambda x: json.loads(x)).cache()
-print 'json lay count', json_lay.count()
+#json_lay = lay.map(lambda x: json.loads(x)).cache()
+#print 'json lay count', json_lay.count()
 
-pairs = json_lay.flatMap(lambda x: [(x['sender'],term) for term in x['text'].split()])
-grouped = pairs.groupBy(lambda x: x)
-counts = [(x, len(y)) for (x, y) in grouped.collect()]
-para_counts = sc.parallelize(counts)
-print 'tf_counts', para_counts.take(5)
+#pairs = json_lay.flatMap(lambda x: [(x['sender'],term) for term in x['text'].split()])
+#grouped = pairs.groupBy(lambda x: x)
+#counts = [(x, len(y)) for (x, y) in grouped.collect()]
+#para_counts = sc.parallelize(counts)
+#print 'tf_counts', para_counts.take(5)
 
 
 email_term_pairs = json_lay.flatMap(lambda x: [(term, x['text']) for term in x['text'].split()])
 email_term_pairs_distinct = email_term_pairs.distinct()
 email_pairs_grouped = email_term_pairs_distinct.groupBy(lambda x: x[0])
-idf_counts = [(x, len(y)) for (x, y) in email_pairs_grouped.collect()]
-para_idf_counts = sc.parallelize(idf_counts)
+#idf_counts = [(x, len(y)) for (x, y) in email_pairs_grouped.collect()]
+#para_idf_counts = sc.parallelize(idf_counts)
 print 'idf_counts', para_idf_counts.take(5)
+print email_pairs_grouped.collect()[:4]
 
 # How to use a join to combine two datasets.
 frequencies = sc.parallelize([('a', 2), ('the', 3)])
