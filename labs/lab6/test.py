41a7ae8e9f8384c4a6c91a11de4dda9022e57353
Brian Bell <brian.bell.jr@gmail.com>
2013-10-15 21:13:10 -0400

    try to do idf

--- labs/lab6/test.py
+++ labs/lab6/test.py
@@ -12,60 +12,18 @@ json_lay = lay.map(lambda x: json.loads(x)).cache()
 print 'json lay count', json_lay.count()
 
 pairs = json_lay.flatMap(lambda x: [(x['sender'],term) for term in x['text'].split()])
-print 'pairs', pairs.take(2)
 grouped = pairs.groupBy(lambda x: x)
-print 'grouped', grouped.take(2)
 counts = [(x, len(y)) for (x, y) in grouped.collect()]
 para_counts = sc.parallelize(counts)
-print 'counts', para_counts.take(5)
+print 'tf_counts', para_counts.take(5)
 
 
-
-################
-# all_words = json_lay.flatMap(lambda x: x['text'].split())
-# words = all_words.distinct()
-
-# for word in words.collect()]
-
-# json_lay.map(lambda x: 
-####################
-email_term_pairs = json_lay.flatMap(lambda x: [(term,x) for term in x['text'].split()])
-#print 'email_term_pairs', email_term_pairs.take(1)
-
-group_email_term_pairs = email_term_pairs.groupBy(lambda x: x[0])
-#print 'group_email_term_pairs', group_email_term_pairs.take(1)
-
-dist = group_email_term_pairs.flatMap(lambda x: x[0],len(x[1]))
-print 'dist', dist.take(1)
-
-
-filtered_lay = json_lay.filter(lambda x: 'chairman' in x['text'].lower())
-print 'lay filtered to chairman', filtered_lay.count()
-
-to_list = json_lay.flatMap(lambda x: x['to'])
-print 'to_list', to_list.take(5)
-
-#grab senders
-#pairs  = json_lay.map(lambda x: {'term': term, 'author': x['sender']} for term in x['text'])
-		# pairs = json_lay.flatMap(lambda x: [(x['sender'],term) for term in x['text'].split()])
-		# groups = pairs.groupBy(lambda x: x)
-		# print groups.take(1)
-		# counts = pairs.map(lambda x: (x[0],len(x[1])))
-#sender_group = json_lay.groupBy(lambda x: x['sender'])
-#sender_group = json_lay.map(lambda x: x[])
-
-#sender_message = [[(group[0],elem) for elem in group[1]] for group in result]
-#sender_terms = 
-#senders = json_lay.map(lambda x: x['sender']).distinct()
-#collected = senders.collect()
-		# print 'pairs_list', counts.take(3)
-#terms = json_lay.filter(lambda x: _ in x['text'].lower())
-#flat = terms.flatMap(lambda x: x)
-
-		# counted_values = to_list.countByValue()
-# Uncomment the next line to see a dictionary of every `to` mapped to
-# the number of times it appeared.
-#print 'counted_values', counted_values
+email_term_pairs = json_lay.flatMap(lambda x: [{'term': term, 'text': x['text']} for term in x['text'].split()])
+email_term_pairs_distinct = email_term_pairs.distinct()
+email_pairs_grouped = email_term_pairs_distinct.groupBy(lambda x: x['term'])
+idf_counts = [(x, len(y)) for (x, y) in email_pairs_grouped.collect()]
+para_idf_counts = sc.parallelize(idf_counts)
+print 'idf_counts', para_idf_counts.take(5)
 
 # How to use a join to combine two datasets.
 frequencies = sc.parallelize([('a', 2), ('the', 3)])
