4b0db01b5705e0981fc350784ce3e3c7d208bd1c
Brian Bell <brian.bell.jr@gmail.com>
2013-10-15 21:41:14 -0400

    try to do idf

--- labs/lab6/test.py
+++ labs/lab6/test.py
@@ -21,10 +21,10 @@ json_lay = lay.map(lambda x: json.loads(x)).cache()
 email_term_pairs = json_lay.flatMap(lambda x: [(term, x['text']) for term in x['text'].split()])
 email_term_pairs_distinct = email_term_pairs.distinct()
 email_pairs_grouped = email_term_pairs_distinct.groupBy(lambda x: x[0])
-#idf_counts = [(x, len(y)) for (x, y) in email_pairs_grouped.collect()]
+idf_counts = email_pairs_grouped.flatMap(lambda group: [(x, len(y)) for (x, y) in group])
 #para_idf_counts = sc.parallelize(idf_counts)
-#print 'idf_counts', para_idf_counts.take(5)
-print email_pairs_grouped.collect()[:4]
+print 'idf_counts', para_idf_counts.take(5)
+#print email_pairs_grouped.collect()[:4]
 
 # How to use a join to combine two datasets.
 frequencies = sc.parallelize([('a', 2), ('the', 3)])
