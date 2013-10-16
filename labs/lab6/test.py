8ad5e89e31f2b0744f2bf8c13d2f2b549463c8ce
Brian Bell <brian.bell.jr@gmail.com>
2013-10-15 22:19:32 -0400

    try to do idf

--- labs/lab6/test.py
+++ labs/lab6/test.py
@@ -18,13 +18,13 @@ json_lay = lay.map(lambda x: json.loads(x)).cache()
 #para_counts = sc.parallelize(counts)
 #print 'tf_counts', para_counts.take(5)
 
-email_term_pairs = json_lay.flatMap(lambda x: [(term, hashlib.sha224(x['text']).hexdigest()) for term in x['text'].split()])
+email_term_pairs = json_lay.flatMap(lambda x: [(term.lower(), hashlib.sha224(x['text']).hexdigest()) for term in x['text'].split()])
 email_term_pairs_distinct = email_term_pairs.distinct()
 email_pairs_grouped = email_term_pairs_distinct.groupBy(lambda x: x[0])
 idf_counts = [(x, len(y)) for (x, y) in email_pairs_grouped.collect()]
 # idf_counts = email_pairs_grouped.flatMap(lambda x: (x[0], len(x[1])))
 para_idf_counts = sc.parallelize(idf_counts)
-print 'idf_counts', para_idf_counts.take(30)
+print 'idf_counts', para_idf_counts.take(50)
 #print email_pairs_grouped.collect()[:4]
 
 # How to use a join to combine two datasets.
