f57ef13eae165f38daffab949cbde63d5a5740c6
Brian Bell <brian.bell.jr@gmail.com>
2013-10-16 01:12:51 -0400

    try to do idf

--- labs/lab6/test.py
+++ labs/lab6/test.py
@@ -9,7 +9,7 @@ import re
 print 'loading'
 sc = SparkContext("spark://ec2-54-200-174-121.us-west-2.compute.amazonaws.com:7077", "Simple App")
 # Replace `lay-k.json` with `*.json` to get a whole lot more data.
-lay = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/enron/lay-k.json')
+lay = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/enron/*.json')
 
 # Load JSON
 json_lay = lay.map(lambda x: json.loads(x)).cache()
@@ -34,23 +34,23 @@ join_map = join_ifidf.map(lambda x: ( (x[0], x[1][0][0], x[1][0][1]*x[1][1] )))
 print 'join', join_map.take(10)
 
 # Sender disambiguation using regex
-filter_ken = join_map.filter(lambda x: re.match(r'(kenneth|lay).*', x[1]))
+filter_ken = join_map.filter(lambda x: re.match(r'(ken.lay|kenneth.lay|lay.ken).*', x[1]))
 print 'ken', sorted(filter_ken.collect(), key=lambda x: x[2], reverse=True)[:10]
 
-filter_jeff = join_map.filter(lambda x: re.match(r'(jeff|skilling).*', x[1]))
+filter_jeff = join_map.filter(lambda x: re.match(r'(jeff.skilling|jefferey.skilling|skilling.jeff).*', x[1]))
 print 'jeff', sorted(filter_jeff.collect(), key=lambda x: x[2], reverse=True)[:10]
 
-filter_andrew = join_map.filter(lambda x: re.match(r'(andrew|fastow).*', x[1]))
+filter_andrew = join_map.filter(lambda x: re.match(r'(andrew.fastow|andy.fastow|fastow.andy).*', x[1]))
 print 'andrew', sorted(filter_andrew.collect(), key=lambda x: x[2], reverse=True)[:10]
 
 
 
 # How to use a join to combine two datasets.
-frequencies = sc.parallelize([('a', 2), ('the', 3)])
-inverted_index = sc.parallelize([('a', ('doc1', 5)), ('the', ('doc1', 6)), ('cats', ('doc2', 1)), ('the', ('doc2', 2))])
+#frequencies = sc.parallelize([('a', 2), ('the', 3)])
+#inverted_index = sc.parallelize([('a', ('doc1', 5)), ('the', ('doc1', 6)), ('cats', ('doc2', 1)), ('the', ('doc2', 2))])
 
 # See also rightOuterJoin and leftOuterJoin.
-join_result = frequencies.join(inverted_index)
+#join_result = frequencies.join(inverted_index)
 
 # If you don't want to produce something as confusing as the next
 # line's [1][1][0] nonesense, represent your data as dictionaries with
