f0652d451504c82a658c7c5d4c6fb5d73987416b
Brian Bell <brian.bell.jr@gmail.com>
2013-10-15 22:05:00 -0400

    try to do idf

--- labs/lab6/test.py
+++ labs/lab6/test.py
@@ -22,9 +22,10 @@ m = hashlib.md5()
 email_term_pairs = json_lay.flatMap(lambda x: [(term, hashlib.sha224(x['text']).hexdigest()) for term in x['text'].split()])
 email_term_pairs_distinct = email_term_pairs.distinct()
 email_pairs_grouped = email_term_pairs_distinct.groupBy(lambda x: x[0])
-idf_counts = email_pairs_grouped.flatMap(lambda x: (x[0], len(x[1])))
-#para_idf_counts = sc.parallelize(idf_counts)
-print 'idf_counts', idf_counts.take(1)
+idf_counts = [(x, len(y)) for (x, y) in email_pairs_grouped.collect()]
+# idf_counts = email_pairs_grouped.flatMap(lambda x: (x[0], len(x[1])))
+para_idf_counts = sc.parallelize(idf_counts)
+print 'idf_counts', para_idf_counts.take(3)
 #print email_pairs_grouped.collect()[:4]
 
 # How to use a join to combine two datasets.
